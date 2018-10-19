a70d351bdd572355b622d112cc2d5d4d664806ee
Sandra Anderson <anderson.sandi@gmail.com>
2018-10-19 06:51:36 -0700

    added package and practice set

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,15 @@
 <?php
-
 /*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+ * Home
+ */
+Route::get('/', 'WelcomeController@index');
+/**
+ * Books
+ */
+Route::get('/books/{title}', 'BookController@show');
+Route::get('/books', 'BookController@index');
+/**
+ * Practice
+ */
+Route::any('/practice/{n?}', 'PracticeController@index');
 
-Route::get('/', function () {
-    return view('welcome');
-});
